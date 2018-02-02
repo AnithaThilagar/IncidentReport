011a6541e9a333547bbf39068f8f1ab349059a2d
Anitha T <39959@hexaware.com>
2018-02-02 15:20:59 +0530

    Code commit for response

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ function incidentSubCategory(category) {
                                     }
                                 ]
                             }
+                        ]
                     }
                 }
             }
