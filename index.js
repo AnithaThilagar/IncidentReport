4360e633b872a13a5cb6dc29f36291607d75f4a4
Anitha T <39959@hexaware.com>
2018-02-02 09:43:25 +0530

    Response msg commit

--- index.js
+++ index.js
@@ -19,49 +19,9 @@ const apiaiApp = apiai("1c5c2bd1f8b548b18f3782ca17420f2c");
 app.post('/ai', (req, res) => {
     console.log('*** Inside service now request ***');
     console.log(req.body.result);
-
     if (req.body.result.action === 'input.welcome') {
-        console.log('Inside Welcome intent');
-        /*let msg = 'Hi welcome';
-        return res.json({
-            speech: msg,
-            displayText: msg,
-            source: 'reportIncidentBot'
-        });*/
-        let content = `Hi, welcome to incident Report Bot. \n I can help you with the following.\n 1) To report an incident in your organization \n 2) To view the status of the incidents \n 3) Add comments to the incidents \n Please select any one of the following to continue`;
-        return res.json({
-            speech: '',
-            displayText: "Hi, welcome to incident Report Bot",
-            data: {
-                "facebook": {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": [
-                                {
-                                    "title": "Report Incident Bot",
-                                    "subtitle": content,
-                                    "buttons": [
-                                        {
-                                            "type": "postback",
-                                            "title": "Report Incident",
-                                            "payload": "report_incident"
-                                        },
-                                        {
-                                            "type": "postback",
-                                            "title": "My Incidents",
-                                            "payload": "my_incidents"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                }
-            },
-            source: 'reportIncidentBot'
-        });
+        console.log('Inside Welcome intent');       
+        welcomeIntent();
     } else {
         console.log('Other than welcome intent');
         let msg = "Can't understand";
@@ -72,3 +32,40 @@ app.post('/ai', (req, res) => {
         });
     }
 });
+
+//To send the welcome intent for the bot as the button
+function welcomeIntent() {    
+    return res.json({
+        speech: '',
+        displayText: "Hi, welcome to incident Report Bot",
+        data: {
+            "facebook": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "Report Incident Bot",
+                                "subtitle": 'Hi, welcome to incident Report Bot. \n I can help you with the following.\n 1) To report an incident in your organization \n 2) To view the status of the incidents \n 3) Add comments to the incidents \n Please select any one of the following to continue',
+                                "buttons": [
+                                    {
+                                        "type": "postback",
+                                        "title": "Report Incident",
+                                        "payload": "report_incident"
+                                    },
+                                    {
+                                        "type": "postback",
+                                        "title": "My Incidents",
+                                        "payload": "my_incidents"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        source: 'reportIncidentBot'
+    });
+}
