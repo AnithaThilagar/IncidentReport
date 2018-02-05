22d0bcd9328d04a3fedb7e752431813c94a60ead
Anitha T <39959@hexaware.com>
2018-02-05 13:58:54 +0530

    log commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let userData = {};
 //To handle the response to bot
 app.post('/ai', (req, res) => {
     console.log('*** Inside service now request ***');
-    console.log(JSON.stringify(req));
+    console.log(req);
     if (req.body.result.action === 'input.welcome') {
         console.log('Inside Welcome');       
         return res.json(welcomeIntent());
