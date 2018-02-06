5d77ad5fa5530ab53e5d71cbfb75bb2106f8f2e3
Anitha T <39959@hexaware.com>
2018-02-06 09:30:47 +0530

    Code commit for the rich message - list

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const request = require('request');
 const apiai = require('apiai');
 const GlideRecord = require('servicenow-rest').gliderecord;
 var record = new GlideRecord('dev18442', 'incident', '33238', 'abc123', 'v1');
-console.log('Record ' + JSON.stringify(record));
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,17 +21,13 @@ let userData = {};
 
 //To handle the response to bot
 app.post('/ai', (req, res) => {
-    console.log('*** Inside service now request ***');
     console.log(req.body.result);
     if (req.body.result.action === 'input.welcome') {
-        console.log('Inside Welcome');       
         return res.json(welcomeIntent());
     } else if (req.body.result.action === 'reportIncident') {
-        console.log('Inside report incident');
         return res.json(incidentCategory());
     } else if (req.body.result.action === 'incident-category') {
         userData.category = req.body.result.resolvedQuery;
-        console.log('Inside incident sub category');
         return res.json(incidentSubCategory(req.body.result.resolvedQuery.toLowerCase()));
     } else if (req.body.result.action === 'incident-subcategory') {
         userData.subCategory = req.body.result.resolvedQuery;
@@ -43,11 +38,10 @@ app.post('/ai', (req, res) => {
     } else if (req.body.result.action === 'IncidentSubcategory.IncidentSubcategory-modeOfContact.IncidentSubcategory-modeOfContact-getModeOfContact') {
         userData.modeOfContact = req.body.result.resolvedQuery;
         return res.json(incidentContactDetails(req.body.result.resolvedQuery.toLowerCase()));
-    } else if (req.body.result.action === 'incidentCreation') {
+    } else if (req.body.result.action === 'getPhoneNumber' || req.body.result.action === 'getMailId') {
         userData.contactDetails = req.body.result.resolvedQuery;
         return res.json(saveIncident());
     } else {
-        console.log('Other than welcome intent');
         let msg = "Can't understand";
         return res.json({
             speech: msg,
@@ -117,65 +111,54 @@ function incidentCategory() {
 
 //To send the sub category for the value for the incident category selected as list
 function incidentSubCategory(category) {
-    if (category == 'hardware') {
-        return {
-            speech: '',
-            displayText: "Hi, welcome to incident Report Bot",
-            data: {
-                "facebook": {
-                    "text": "Please select any one sub-category",
-                    "quick_replies": [
+    return {
+        "facebook": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
                         {
-                            "content_type": "text",
                             "title": "New Device",
-                            "payload": "New Device"
+                            "image_url": "https://cdn3.iconfinder.com/data/icons/phones-set-2/512/27-512.png",
+                            "subtitle": "For requesting new device",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "New Device",
+                                    "payload": "new_device"
+                                }
+                            ]
                         },
                         {
-                            "content_type": "text",
                             "title": "Damaged Device",
-                            "payload": "Damaged Device"
+                            "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxod-I0fuatggTIxbnHFELF6y62zwXkrzthtoVAWOmOwNQuPJusw",
+                            "subtitle": "To report the device is damaged",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Damaged Device",
+                                    "payload": "damaged_device"
+                                }
+                            ]
                         },
                         {
-                            "content_type": "text",
                             "title": "Replace Device",
-                            "payload": "Replace Device"
+                            "image_url": "https://cdn3.iconfinder.com/data/icons/finance-and-money-1/512/arrows_currency_exchange_direction_flat_icon-512.png",
+                            "subtitle": "For replacing the device",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Replace Device",
+                                    "payload": "replace_device"
+                                }
+                            ]
                         }
                     ]
                 }
-            },
-            source: 'reportIncidentBot'
-        }; 
-    } else if (category == 'software') {
-        return {
-           speech: '',
-           displayText: "Hi, welcome to incident Report Bot",
-           data: {
-                "facebook": {
-                    "text": "Please select any one sub-category",
-                    "quick_replies": [
-                        {
-                            "content_type": "text",
-                            "title": "Software Installation",
-                            "payload": "Software Installation"
-                        },
-                        {
-                            "content_type": "text",
-                            "title": "Problem with Installed Software",
-                            "payload": "Problem with Installed Software"
-                        }
-                    ]
-                }
-           },
-           source: 'reportIncidentBot'
-        };
-    } else {
-        let msg = "Others";
-        return res.json({
-            speech: msg,
-            displayText: msg,
-            source: 'reportIncidentBot'
-        });
-    }
+            }
+        }
+    };
 }
 
 //To handle the urgency type for the incidents
@@ -324,3 +307,63 @@ function saveIncident() {
             }
         }
     };*/
+
+/*if (category == 'hardware') {
+    return {
+        speech: '',
+        displayText: "Hi, welcome to incident Report Bot",
+        data: {
+            "facebook": {
+                "text": "Please select any one sub-category",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "New Device",
+                        "payload": "New Device"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Damaged Device",
+                        "payload": "Damaged Device"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Replace Device",
+                        "payload": "Replace Device"
+                    }
+                ]
+            }
+        },
+        source: 'reportIncidentBot'
+    };
+} else if (category == 'software') {
+    return {
+        speech: '',
+        displayText: "Hi, welcome to incident Report Bot",
+        data: {
+            "facebook": {
+                "text": "Please select any one sub-category",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Software Installation",
+                        "payload": "Software Installation"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Problem with Installed Software",
+                        "payload": "Problem with Installed Software"
+                    }
+                ]
+            }
+        },
+        source: 'reportIncidentBot'
+    };
+} else {
+    let msg = "Others";
+    return res.json({
+        speech: msg,
+        displayText: msg,
+        source: 'reportIncidentBot'
+    });
+}*/
