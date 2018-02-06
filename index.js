6cd9dab9398424f7913c169f12d14a0344ee84c7
Anitha T <39959@hexaware.com>
2018-02-06 16:30:39 +0530

    Check

--- index.js
+++ index.js
@@ -329,8 +329,8 @@ function getIncidentDetails(incidentId) {
         method: 'GET'
     }, (error, response) => {
         if (!error && response.statusCode == 200) {
-            console.log("Inside response build " + response.body.result);
-            let incidentDetails = "<table border = '0'><tr><th>Incident Number</th><td>" + response.body.result + "</td></tr></table>";
+            console.log(response.body);
+            let incidentDetails = "<table border = '0'><tr><th>Incident Number</th><td>" + response.body + "</td></tr></table>";
             return {
                 speech: incidentDetails,
                 displayText: incidentDetails,
