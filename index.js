e61bf346fa4bc9d33c8f26c429d2ae49e275e9e8
Anitha T <39959@hexaware.com>
2018-02-08 11:16:12 +0530

    Code commit for the welcome intent

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post('/ai', (req, res) => {
 
 //To send the welcome message for the bot as the button
 function welcomeIntent() {    
-    return {
+    /*return {
         speech: '',
         displayText: "Hi, welcome to incident Report Bot",
         data: {
@@ -113,7 +113,7 @@ function welcomeIntent() {
                     "type": "template",
                     "payload": {
                         "template_type": "button",
-                        "text":"Hi, welcome to incident Report Bot. <br/> I can help you with the following.<br/> 1) To report an incident in your organization <br/> 2) To view the status of the incidents <br/> 3) Add comments to the incidents <br/> Please select any one of the following to continue",
+                        "text":"Hi, welcome to incident Report Bot. \n I can help you with the following.\n 1) To report an incident in your organization \n 2) To view the status of the incidents \n 3) Add comments to the incidents \n Please select any one of the following to continue",
                         "buttons": [
                             {
                                 "type": "postback",
@@ -131,6 +131,34 @@ function welcomeIntent() {
             }
         },
         source: 'reportIncidentBot'
+    };*/
+    return {
+        speech: '',
+        displayText: '',
+        messages: [
+            {
+                "type": 0,
+                "platform": "facebook",
+                "speech": "Hi, welcome to incident Report Bot. \n I can help you with the following.\n 1) To report an incident in your organization \n 2) To view the status of the incidents \n 3) Add comments to the incidents \n Please select any one of the following to continue"
+            },
+            {
+                "type": 1,
+                "platform": "facebook",
+                "title": "Report It",
+                "subtitle": "Report It - To sort it",
+                "imageUrl": "https://mgtvwlns.files.wordpress.com/2015/05/reportit-logo5b35d.jpg",
+                "buttons": [
+                    {
+                        "text": "Report Incident",
+                        "postback": "Report Incident"
+                    },
+                    {
+                        "text": "My Incident",
+                        "postback": "My Incident"
+                    }
+                ]
+            }
+        ]
     };
 }
 
