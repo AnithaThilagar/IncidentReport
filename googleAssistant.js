9476e4418b3d2aa57a5e3eade5572bf3909a8327
AnithaThilagar <anithathilagar@gmail.com>
2018-02-13 21:20:15 +0530

    Code commit for GA

--- googleAssistant.js
+++ googleAssistant.js
@@ -7,7 +7,7 @@ var googleAssistant = {
                 speech: 'Hi welcome to Report It Bot!',
                 displayText: 'Hi welcome to Report It Bot!'
             })
-            .addBasicCard(app.buildBasicCard('<b>I can help you with <ul><li>reporting incident</li><li>view the status for the incident</li></ul></b>')
+            .addBasicCard(app.buildBasicCard("This is a basic card.  Text in a basic card can include \"quotes\" and most other unicode characters including emoji 📱.  Basic cards also support some markdown formatting like *emphasis* or _italics_, **strong** or __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other things like line  \nbreaks",)
                 .setTitle('Report It - To solve it')
                 .setImage('https://mgtvwlns.files.wordpress.com/2015/05/reportit-logo5b35d.jpg', 'Image alternate text')
                 .setImageDisplay('CROPPED')
