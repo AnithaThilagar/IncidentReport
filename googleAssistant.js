9a7c26eec7801780abf7fccc76d8c0523da11927
AnithaThilagar <anithathilagar@gmail.com>
2018-02-13 20:14:53 +0530

    Code commit for GA

--- googleAssistant.js
+++ googleAssistant.js
@@ -10,9 +10,11 @@ var googleAssistant = {
             .addBasicCard(app.buildBasicCard('<b>I can help you with <ul><li>reporting incident</li><li>view the status for the incident</li></ul></b>')
                 .setTitle('Report It - To solve it')
                 .setImage('https://mgtvwlns.files.wordpress.com/2015/05/reportit-logo5b35d.jpg', 'Image alternate text')
-                .setImageDisplay('CROPPED')
+                .addButton('Report Incident', 'Report Incident')
+				.addButton('My Incidents', 'My Incidents')
+				.setImageDisplay('CROPPED')
             )
-            .addSuggestions(['Report Incident', 'My Incidents']));
+            //.addSuggestions(['Report Incident', 'My Incidents']));
 	}
 };
 
