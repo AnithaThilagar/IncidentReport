5915169e6762c031dea1a49b462e680fc9e73328
Anitha T <39959@hexaware.com>
2018-02-19 11:01:54 +0530

    Code commit for the default response

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/ai', (req, res) => {
             case "google": handleGoogleResponse(req, res);
         }
     } else {
-        console.log(req.body.originalRequest.source);
+        console.log('Req from other sources');
         handleFacebook(req, res);
     }
 });
