3193235ca74d982007dd4a1109ec41faebb517ed
Anitha T <39959@hexaware.com>
2018-02-20 16:05:10 +0530

    Code commit for skype

--- skype.js
+++ skype.js
@@ -33,7 +33,16 @@ var skype = {
     },
     //To send the incident category as quick replies
     incidentCategory: function () {
-        return {
+        var heroCard = new HeroCard
+        {
+            Title = "BotFramework Hero Card",
+                Subtitle = "Your bots — wherever your users are talking",
+                Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
+                Images = new List < CardImage > { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
+                Buttons = new List < CardAction > { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") }
+        };
+        return heroCard.ToAttachment();
+        /*return {
             speech: '',
             displayText: "Hi, welcome to incident Report Bot",
             data: {
@@ -54,7 +63,7 @@ var skype = {
                 }
             },
             source: 'reportIncidentBot'
-        };
+        };*/
     },
     //To send the sub category for the value for the incident category selected as list
     incidentSubCategory: function (category) {
