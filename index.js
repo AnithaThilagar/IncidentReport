f02ad03da772af49d661a16ef4c5203573b84b35
Anitha T <39959@hexaware.com>
2018-02-27 08:46:22 +0530

    log code commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let userData = {};
 //To handle the response to bot
 app.post('/ai', (req, res) => {
     console.log("Inside the API handle ");
-    console.log(.body.originalRequest);
+    console.log(req.body.originalRequest);
     let source = '';
     if (typeof req.body.originalRequest != "undefined") {
         console.log(req.body.originalRequest.source);
