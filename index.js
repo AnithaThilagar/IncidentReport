0f51ade9cd2fca56697645e16a7ca816d15c62f0
AnithaThilagar <anithathilagar@gmail.com>
2018-03-12 10:08:17 +0530

    Chatlog commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ try {
     var logger = new winston.Logger({
         level: 'debug',
         transports: [
-            new (winston.transports.Console)()
+            new (winston.transports.Console)(),
             new (winston.transports.File)({ filename: './sample.log' })
         ]
     });
