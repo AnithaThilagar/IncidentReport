4e13a8782049e8679ce761e448f01ed9ec0d010f
AnithaThilagar <anithathilagar@gmail.com>
2018-03-15 12:26:01 +0530

    Chatlog commit

--- logger.js
+++ logger.js
@@ -64,9 +64,9 @@ try {
         S3StreamLogger = require('s3-streamlogger').S3StreamLogger,
         s3_stream = new S3StreamLogger({
             //signatureVersion: 'v4',
-            bucket: "testchatlog",
-            access_key_id: "AKIAJVGUJOAJX7FNVH3Q",
-            secret_access_key: "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",
+            bucket: process.env.AWS_BUCKET_NAME,
+            access_key_id: process.env.AWS_ACCESS_KEY,
+            secret_access_key: process.env.AWS_SECRET_KEY,
             name_format: "%Y-%m-%d-%H-%M-%S-%L-chatlogs-reportIncident.log"
             //compress: true                       //To compress to log.gz format
         });
